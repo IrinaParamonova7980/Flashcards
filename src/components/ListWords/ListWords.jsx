14c77b40f1a6a66dec394d0ee9b6acdef9426d30
IrinaParamonova7980 <irisha-7980@yandex.ru>
2023-04-03 10:40:07 +0300

    item.english

--- src/components/ListWords/ListWords.jsx
+++ src/components/ListWords/ListWords.jsx
@@ -9,6 +9,17 @@ export default function ListWords() {
   const [editing, editWord] = useState(true);
   const [userData, newValue] = useState(data);
 
+  function onChangeWord(e) {
+    const changeWord = userData.map((item) => {
+      if (item.english) {
+        return { ...item, english: (item.english = e.target.value) };
+      } else {
+        return item;
+      }
+    });
+    newValue(changeWord);
+  }
+
   return (
     <table className={styles.table}>
       <caption className={styles.heading}>Список слов</caption>
@@ -32,19 +43,7 @@ export default function ListWords() {
                   type="text"
                   className={styles.input}
                   value={data.english}
-                  onChange={(e) => newValue(e.target.value)}
-
-                  // onChange={newValue(
-                  //   userData.map((item) => (
-                  //     <td>
-                  //       <input
-                  //         type="text"
-                  //         className={styles.input}
-                  //         value={(e) => e.target.value}
-                  //       ></input>
-                  //     </td>
-                  //   ))
-                  // )}
+                  onChange={onChangeWord}
                 ></input>
               </td>
             )}
