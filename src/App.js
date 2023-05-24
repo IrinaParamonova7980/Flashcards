13268954a6fadbf170979f5b52d7d63cdbf62a76
IrinaParamonova7980 <irisha-7980@yandex.ru>
2023-05-24 08:03:11 +0300

    fetch in App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import ListWords from "./components/listWords/ListWords";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Carousel from "./components/carousel/Carousel";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoMatch from "./components/nomatch/NoMatch";
+import { WordContext } from "./components/wordsContext/WordContext";
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://itgirlschool.justmakeit.ru/api/words")
+      .then((responce) => {
+        if (responce.ok) {
+          return responce.json();
+        }
+        throw responce;
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error</p>;
+  }
+
   return (
-    <Router>
-      <div className="App">
-        <header>
-          <Header></Header>
-        </header>
+    <WordContext.Provider value={data}>
+      <Router>
+        <div className="App">
+          <header>
+            <Header></Header>
+          </header>
 
-        <main>
-          <Routes>
-            <Route exact path="/" element={<ListWords />} />
-            <Route path="/game" element={<Carousel index={0} />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </main>
+          <main>
+            <Routes>
+              <Route exact path="/" element={<ListWords />} />
+              <Route path="/game" element={<Carousel index={0} />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </main>
 
-        <footer>
-          <Footer></Footer>
-        </footer>
-      </div>
-    </Router>
+          <footer>
+            <Footer></Footer>
+          </footer>
+        </div>
+      </Router>
+    </WordContext.Provider>
   );
 }
--- src/components/wordsContext/WordContext.jsx
+++ src/components/wordsContext/WordContext.jsx
@@ -1,8 +1,2 @@
-import React from "react";
-const WordsContext = React.createContext();
-
-function WordsContextProvider(props) {
-    
-}
-
-export { WordsContextProvider, WordsContext };
+import { createContext } from "react";
+export const WordContext = createContext();
