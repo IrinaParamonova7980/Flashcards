d5fa97e8b03561b92561e3681626a43aefa6e90a
IrinaParamonova7980 <irisha-7980@yandex.ru>
2023-05-25 06:53:29 +0300

    стилизация добавления слова

--- src/components/ListWords/ListWords.jsx
+++ src/components/ListWords/ListWords.jsx
@@ -1,10 +1,35 @@
 import styles from "./listWords.module.scss";
 import TableRow from "../tableRow/TableRow";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { WordContext } from "../wordsContext/WordContext";
 
 export default function ListWords() {
   const { data } = useContext(WordContext);
+  const [writeWord, setWriteWord] = useState(false);
+  const [valueUser, setValueUser] = useState("");
+  const [wordList, setWordList] = useState(data);
+  const [newWords, setNewWords] = useState({});
+
+  const handleChange = (e) => {
+    setValueUser({ ...valueUser, [e.target.name]: e.target.value });
+  };
+
+  let newWord;
+
+  // function addWord() {
+  //   newWord = {
+  //     id: data.id + 1,
+  //     english: e.target.value,
+  //     transcription: e.target.value,
+  //     russian: e.target.value,
+  //     tags: e.target.value,
+  //   };
+
+  //   setWordList([...wordList, newWord]);
+  //   setNewWords("");
+  // }
+
 
   return (
     <div className={styles.listwords_block}>
@@ -23,8 +48,78 @@ export default function ListWords() {
           {data.map((item) => (
             <TableRow key={item.id} {...item}></TableRow>
           ))}
+          {writeWord ? (
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  name="english"
+                  className={styles.input}
+                  value={valueUser.english}
+                  onChange={handleChange}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="transcription"
+                  className={styles.input}
+                  value={valueUser.transcription}
+                  onChange={handleChange}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="russian"
+                  className={styles.input}
+                  value={valueUser.russian}
+                  onChange={handleChange}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="tags"
+                  className={styles.input}
+                  value={valueUser.tags}
+                  onChange={handleChange}
+                ></input>
+              </td>
+              <td className={styles.button_block}>
+                <button
+                  className={styles.button_save}
+                  onClick={() => {
+                    addWord;
+                  }}
+                >
+                  Добавить
+                </button>
+                <button
+                  className={styles.button_cancel}
+                  onClick={() => {
+                    setWriteWord(false);
+                    setValueUser("");
+                  }}
+                >
+                  Отменить
+                </button>{" "}
+              </td>
+            </tr>
+          ) : (
+            ""
+          )}
         </tbody>
       </table>
+
+      <button
+        className={styles.button}
+        onClick={() => {
+          setAddWord(true);
+        }}
+      >
+        Добавить слово
+      </button>
     </div>
   );
 }
